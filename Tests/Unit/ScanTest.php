96d92d9c754cf0b54c9bc2b38f821426d888636c
kedniko <42846752+kedniko@users.noreply.github.com>
2023-05-30 01:23:16 +0200

    test: scan

---
+++ Tests/Unit/ScanTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests;
+
+use Kedniko\Vivy\V;
+
+uses()->group('scan');
+
+test('scan', function () {
+	V::registerPlugin(new \Kedniko\Vivy\Plugins\StandardLibrary\StandardLibrary());
+	(new \Kedniko\Vivy\Commands\ScanCommand())->handle('_ide_helper_vivy.php');
+
+	$fileExists = file_exists('_ide_helper_vivy.php');
+	expect($fileExists)->toBeTrue();
+});
